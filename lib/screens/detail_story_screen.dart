4a2242136a1758b2fe0da572e3f6ebee0d890699
rizrmdhn <rizrmdhn.work@gmail.com>
2023-11-05 18:53:13 +0800

    change detail story layout

--- lib/screens/detail_story_screen.dart
+++ lib/screens/detail_story_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:story_app/components/my_app_bar.dart';
 import 'package:story_app/provider/story_provider.dart';
 import 'package:timezone/timezone.dart' as tz;
 
@@ -17,17 +18,29 @@ class DetailStoryScreen extends StatelessWidget {
       builder: (context, value, child) => value.isFetching
           ? const Center(child: CircularProgressIndicator())
           : Material(
-              child: NestedScrollView(
-                headerSliverBuilder: (context, innerBoxIsScrolled) {
-                  return [
-                    SliverAppBar(
-                      expandedHeight: 200,
-                      pinned: true,
-                      flexibleSpace: FlexibleSpaceBar(
-                        // add favorite button in image background
-                        background: Stack(
-                          children: [
-                            Image.network(
+              child: Scaffold(
+                appBar: const MyAppBar(title: 'Detail Story'),
+                // scroll view
+                body: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  height: MediaQuery.of(context).size.height,
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 20,
+                        right: 20,
+                        left: 20,
+                        bottom: 10,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // image
+                          SizedBox(
+                            height: 250,
+                            child: Image.network(
                               value.detailStory.photoUrl,
                               fit: BoxFit.cover,
                               width: MediaQuery.of(context).size.width,
@@ -49,49 +62,7 @@ class DetailStoryScreen extends StatelessWidget {
                                 );
                               },
                             ),
-                            Positioned(
-                              bottom: 0,
-                              child: Container(
-                                width: MediaQuery.of(context).size.width,
-                                height: 50,
-                                decoration: const BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.bottomCenter,
-                                    end: Alignment.topCenter,
-                                    colors: [
-                                      Colors.black,
-                                      Colors.transparent,
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        title: Text(
-                          value.detailStory.lat.toString(),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.transparent,
                           ),
-                        ),
-                      ),
-                    ),
-                  ];
-                },
-                // scroll view
-                body: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: SingleChildScrollView(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 10, right: 20, left: 20, bottom: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
                           // title
                           Container(
                             margin: const EdgeInsets.only(top: 10),
